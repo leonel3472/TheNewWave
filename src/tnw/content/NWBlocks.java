a830185a3d3e7b5919ac5dcf47cecf8d65a3c0c0
1237 <92003875+12three7@users.noreply.github.com>
2022-08-01 14:58:44 +0800

    Update NWBlocks.java

--- src/tnw/content/NWBlocks.java
+++ src/tnw/content/NWBlocks.java
@@ -4,15 +4,40 @@ import mindustry.world.meta.*;
 import mindustry.type.*;
 import mindustry.ctype.*;
 import mindustry.world.*;
+import mindustry.content.*;
+import mindustry.world.*;
+import mindustry.world.draw.*;
+import mindustry.world.blocks.defense.turrets.*;
+import mindustry.world.blocks.environment.*;
+import tnw.blocks.defense.*;
 
 public class NWBlocks {
-	public static Block start;
+	public static Block 
+	// start
+	start,
+	// wall
+	maraniumWall, maraniumWallLarge;
 	public static void load(){
+		// start
 		// code borrowed from project oblivion
 		start = new Block("start"){{
 			alwaysUnlocked = true;
 			buildVisibility = BuildVisibility.debugOnly;
 			inEditor = false;
+		}};
+		// wall
+		maraniumWall = new BulletWall("maranium-wall"){{
+			requirements(defense, with(maranium, 8));
+			bullet(WBullets.maraniumWallBullet);
+			shots = 4;
+			scaledHealth = 105;
+		}};
+		maraniumWallLarge = new BulletWall("maranium-wall-large"){{
+			requirements(defense, with(maranium, 8));
+			bullet(WBullets.maraniumWallBullet);
+			size = 2;
+			shots = 4;
+			scaledHealth = 105;
 		}};
 	}
 }
