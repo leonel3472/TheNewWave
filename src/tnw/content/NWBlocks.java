5f07f4fbd7c2d44dc59f850beeb9e90b034a2e9f
Drifted Notes <109656172+driftednotes@users.noreply.github.com>
2022-08-01 13:52:54 +0800

    Update NWBlocks.java

--- src/tnw/content/NWBlocks.java
+++ src/tnw/content/NWBlocks.java
@@ -1,12 +1,18 @@
 package tnw.content;
 
+import mindustry.world.meta.*;
+import mindustry.type.*;
+import mindustry.ctype.*;
 import mindustry.world.*;
 
 public class NWBlocks {
 	public static Block start;
 	public static void load(){
+		// code borrowed from project oblivion
 		start = new Block("start"){{
 			alwaysUnlocked = true;
+			buildVisibility = BuildVisibility.debugOnly;
+			inEditor = false;
 		}};
 	}
 }
