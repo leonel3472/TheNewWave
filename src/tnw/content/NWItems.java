a0d27494f33d26a065174eca3f9c876ec927a9a2
1237 <92003875+12three7@users.noreply.github.com>
2022-08-01 14:58:23 +0800

    Update NWItems.java

--- src/tnw/content/NWItems.java
+++ src/tnw/content/NWItems.java
@@ -5,7 +5,7 @@ import arc.struct.Seq;
 import mindustry.type.*;
 
 public class NWItems {
-	public static Item maranium
+	public static Item maranium;
 	public static void load(){
 		maranium = new Item("maranium", Color.valueOf("EBB778FF")){{
 			cost = 0.01f;
