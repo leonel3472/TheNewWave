7834f507f409205743fa272855e02ff06e8bbd4a
1237 <92003875+12three7@users.noreply.github.com>
2022-08-02 21:03:45 +0800

    Create NWBlocks.java

---
+++ src/tnw/content/NWBlocks.java
@@ -0,0 +1,27 @@
+package tnw.content;
+
+import mindustry.world.*;
+import mindustry.world.blocks.*;
+import mindustry.world.blocks.defense.turrets.*;
+import mindustry.world.meta.*;
+import mindustry.content.*;
+import mindustry.graphics.*;
+import static mindustry.type.ItemStack.*;
+
+public class NWBlocks {
+	public static Block
+	// turret
+	root;
+
+	public static void load(){
+		root = new ItemTurret("root"){{
+			requirements(Category.turret, with(Items.copper, 28, Items.lead, 15));
+			// todo ammo(Items.copper, Items.lead, Items.graphite);
+			shootCone = 15;
+			inaccuracy = 2;
+			rotateSpeed = 9;
+			ammoUseEffect = Fx.casing1;
+			recoil = 1.25f;
+		}};
+	}
+}
